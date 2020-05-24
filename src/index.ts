54c67daadc5e38b4db5aa4993bb33339aaefd283
aditi.thakkar <aditi.thakkar@beteasy.com.au>
2020-05-24 16:03:56 +1000

    chore: 1: moved file read into spererate class 2: update .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-import fs from 'fs';
+import {CsvFileReader} from './CsvFileReader'
 
-const matches = fs.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n').map((row: string): string[] => {
-    return row.split(',')
-});
+const reader = new CsvFileReader('football.csv')
+reader.read();
 
 enum MatchResult {
   HomeWin = 'H',
@@ -13,11 +10,11 @@ enum MatchResult {
 }
 let manUnitedWins = 0;
 
-for(let match of matches){
+for(let match of reader.data){
     if(match[1] === 'Man United' && match[5] === MatchResult.HomeWin){
         manUnitedWins++;
     }else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
         manUnitedWins++;
     }
 }
-console.log(`Man united won ${manUnitedWins} games`);
+console.log(`Man united won is ${manUnitedWins} games`);
