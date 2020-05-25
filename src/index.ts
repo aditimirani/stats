982a88dcb6da1fc829fa7aafc61fa8635a403051
aditi.thakkar <aditi.thakkar@beteasy.com.au>
2020-05-25 15:37:59 +1000

    chore: refactor the code using abstract class and generics

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import {CsvFileReader} from './CsvFileReader'
 import {MatchResult} from './MatchResult';
-const reader = new CsvFileReader('football.csv')
-reader.read();
+import {MatchReader} from './MatchReader';
 
+const reader = new MatchReader('football.csv')
+reader.read();
 
 let manUnitedWins = 0;
 
